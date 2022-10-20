e1e861c6dff45ceaa47e00334ba9b45379113172
unknown <crispim1411@gmail.com>
2022-10-20 09:30:42 -0300

    check user

--- src/database/repository.rs
+++ src/database/repository.rs
@@ -56,6 +56,24 @@ impl IRepository for PostgreSQLRepository {
         events::table.load(&mut self.connection)
     }
 
+    fn get_users_event(&mut self, event_name: &str) -> Result<Vec<User>, DieselError> {
+        users::table
+            .inner_join(events::table)
+            .filter(events::name.like(format!("%{}%", event_name)))
+            .select(users::all_columns)
+            .load(&mut self.connection)
+    }
+
+    fn check_user_in_event(&mut self, mention: &str, event_id: &i32) -> Result<bool, DieselError> {
+        let counter: i64 = 
+            users::table
+                .filter(users::mention.eq(mention)
+                    .and(users::event_id.eq(event_id)))
+                .count()
+                .get_result(&mut self.connection)?;
+        Ok(counter > 0)
+    }
+
     fn insert_event(&mut self, name: &str, creator: &str, server_id: &str) -> Result<usize, DieselError> {  
         let new_event = EventForm { name, creator, server_id };
         diesel::insert_into(events::table)
@@ -66,23 +84,16 @@ impl IRepository for PostgreSQLRepository {
     }
 
     fn insert_user_to_event(&mut self, name: &str, mention: &str, event_name: &str) -> Result<usize, DieselError> {
-        let event;
-        match self.get_event(event_name) {
-            Ok(ev) => event = ev,
-            Err(err) => return Err(err),
+        let event = self.get_event(event_name)?;
+
+        if self.check_user_in_event(mention, &event.id)? { 
+            println!("Usuário já registrado no evento");
+            return Ok(0) 
         }
         let user_to_insert = UserForm { name, mention, event_id: &event.id };
         diesel::insert_into(users::table)
             .values(user_to_insert)
             .on_conflict_do_nothing()
             .execute(&mut self.connection)
-    }
-
-    fn get_users_event(&mut self, event_name: &str) -> Result<Vec<User>, DieselError> {
-        users::table
-            .inner_join(events::table)
-            .filter(events::name.like(format!("%{}%", event_name)))
-            .select(users::all_columns)
-            .load(&mut self.connection)
     }
 }
